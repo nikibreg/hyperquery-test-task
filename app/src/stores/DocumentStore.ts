8ba316e34609a8570590bdf3fa9052eb4d07932c
nikibreg <nbregv@gmail.com>
2021-10-21 23:21:37 +0400

    Add saving documents from the store

--- app/src/stores/DocumentStore.ts
+++ app/src/stores/DocumentStore.ts
@@ -3,14 +3,14 @@ import { action, makeAutoObservable, makeObservable, observable, runInAction } f
 const apiUrl = `http://localhost:3001/v1/documents`;
 
 export interface Document {
-    id: string;
-    body: string;
-    created_at: string;
-    deleted_at: string;
-    ordinal_number: number;
-    parent_id: string;
-    title: string;
-    updated_at: string;
+    id?: string;
+    body?: string;
+    created_at?: string;
+    deleted_at?: string;
+    ordinal_number?: number;
+    parent_id?: string;
+    title?: string;
+    updated_at?: string;
 }
 
 export default class DocumentStore {
@@ -21,7 +21,15 @@ export default class DocumentStore {
         this.fetchDocuments()
     }
 
-    addDocument(document: Document) {
+    addDocument(title: string) {
+        const document: Document = {
+            title
+        } 
+        fetch(`${apiUrl}`, {
+                method: 'POST',
+                body: JSON.stringify(document)
+            })
+            .then(r => r.json());
         this.documents.push(document);
     }
 
