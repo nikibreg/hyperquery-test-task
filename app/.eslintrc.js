fc4325195c159eaf5b8f776800d163555c89b588
nikibreg <nbregv@gmail.com>
2021-10-29 00:02:58 +0400

    Add .eslintrc

---
+++ app/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    "indent": ["error", 2, {"ignoredNodes": ["JSXElement"]}],
+    "react/jsx-indent": ["error", 2]
+  },
+};
